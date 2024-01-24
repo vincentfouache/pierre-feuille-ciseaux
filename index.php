dee040dc347077b42909e375d016d29d4221fceb
Vincent Fouache <vincent.fouache@boulanger.com>
2024-01-24 16:14:39 +0100

    feat(): init

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pierre, Feuille, ciseaux</title>
+</head>
+<body>
+<h1>Pierre, Feuille, ciseaux</h1>
+
+<form method="post">
+    <label for="choice">Choisissez : </label>
+    <select name="choice" id="choice">
+        <option value="Pierre">Pierre</option>
+        <option value="Feuille">Feuille</option>
+        <option value="Ciseaux">Ciseaux</option>
+    </select>
+    <button type="submit">Jouer</button>
+</form>
+
+</body>
+</html>
